d1ec9971891a3550c57cadd1b63c841834b26e05
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-05-24 21:54:20 +0800

    0524

--- mysite/quiz/models.py
+++ mysite/quiz/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Question(models.Model):
     description = models.TextField()
-    options = models.JSONField()  # 儲存選項的 JSON 數據
+    options = models.JSONField()  
 
     def __str__(self):
         return self.description
